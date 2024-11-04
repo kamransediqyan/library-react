24e97354e4861d5e15dcb617e0f1a903e300e2bf
kamransediqyan <kamransediqyan777@gmail.com>
2024-11-04 10:20:01 +0430

    updated appjs and header

--- src/App.js
+++ src/App.js
@@ -8,34 +8,8 @@ import Categories from './pages/Categories';
 import Users from './pages/Users';
 import Reservations from './pages/Reservations';
 
-<<<<<<< Updated upstream
 
-const routes=[
-  { path:'/',label:"Home"},
-  { path:'/books',label:"Books"},
-  { path:'/authors',label:"Authors"},
-  { path:'/categories',label:"Categories"},
-  { path:'/users',label:"Users"},
-  { path:'/reservations',label:"Reservations"},
-]
-function App() {
-    return (
-        <Router>
-            <Header title="Library MIS" routes={routes}/>
-              
-            <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books" element={<Books /> }  />
-            <Route path="/authors" element={<Authors />} />
-            <Route path="/categories" element={<Categories />}  />
-            <Route path="/users" element={<Users />}  />
-            <Route path="/reservations" element={<Reservations />}  />
-          </Routes>
-          
-           
-        </Router>
-    );
-=======
+
 const routes = [
   { path: '/', label: "Home" },
   { path: '/books', label: "Books" },
@@ -60,7 +34,7 @@ function App() {
       </Routes>
     </Router>
   );
->>>>>>> Stashed changes
+
 }
 
 export default App;
